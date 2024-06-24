21da2256a918796b4964f39395023f263b6630d5
Jasmer Singh Sanjotra <jasmer.sanjotra@gmail.com>
2024-06-24 11:11:09 -0500

    Added testing support for lits

--- test_lits.py
+++ test_lits.py
@@ -12,8 +12,8 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 
 
-from utils.dataset_lits import LITSTestDataset
-from utils.utils import test_single_volume, random_split_array
+from utils.dataset_lits import LITSTestDataset, LITSDataset
+from utils.utils import test_single_volume, random_split_array, val_single_volume, test_lits_single
 
 from lib.networks import PVT_GCASCADE, MERIT_GCASCADE
 
@@ -47,7 +47,6 @@ args = parser.parse_args()
 #WILL MODIFY ONCE INFERENCE FUNCTION WORKS IN TRAINING SCRIPT
 
 def inference_lits(args, model, db_test, test_save_path=None):
-    #db_test = args.Dataset(base_dir=args.volume_path, split="test_vol", list_dir=args.list_dir, nclass=args.num_classes)
     testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=1)
     logging.info("{} test iterations per epoch".format(len(testloader)))
     model.eval()
@@ -55,18 +54,18 @@ def inference_lits(args, model, db_test, test_save_path=None):
     for i_batch, sampled_batch in tqdm(enumerate(testloader)):
         h, w = sampled_batch["image"].size()[2:]
         image, label, case_name = sampled_batch["image"], sampled_batch["label"], None
-        metric_i = test_single_volume(image, label, model, classes=args.num_classes, patch_size=[args.img_size, args.img_size],
-                                      test_save_path=test_save_path, case=case_name, z_spacing=1, class_names=args.num_classes)
+        metric_i = test_lits_single(image, label, model, classes=args.num_classes, patch_size=[args.img_size, args.img_size],
+                                      test_save_path=test_save_path, case=case_name, z_spacing=1)
         metric_list += np.array(metric_i)
-        logging.info('idx %d case %s mean_dice %f mean_hd95 %f, mean_jacard %f mean_asd %f' % (i_batch, case_name, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1], np.mean(metric_i, axis=0)[2], np.mean(metric_i, axis=0)[3]))
+        logging.info('idx %d mean_dice %f mean_hd95 %f, mean_jacard %f' % (i_batch, np.mean(metric_i, axis=0)[0], np.mean(metric_i, axis=0)[1], np.mean(metric_i, axis=0)[2]))
     metric_list = metric_list / len(db_test)
-    for i in range(1, args.num_classes):
-        logging.info('Mean class (%d) %s mean_dice %f mean_hd95 %f, mean_jacard %f mean_asd %f' % (i, classes[i-1], metric_list[i-1][0], metric_list[i-1][1], metric_list[i-1][2], metric_list[i-1][3]))
+    # for i in range(1, args.num_classes):
+    #     logging.info('Mean class (%d) mean_dice %f mean_hd95 %f, mean_jacard %f' % (i, metric_list[i-1][0], metric_list[i-1][1], metric_list[i-1][2]))
+    print(metric_list)
     performance = np.mean(metric_list, axis=0)[0]
     mean_hd95 = np.mean(metric_list, axis=0)[1]
     mean_jacard = np.mean(metric_list, axis=0)[2]
-    mean_asd = np.mean(metric_list, axis=0)[3]
-    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f, mean_jacard : %f mean_asd : %f' % (performance, mean_hd95, mean_jacard, mean_asd))
+    logging.info('Testing performance in best val model: mean_dice : %f mean_hd95 : %f, mean_jacard : %f ' % (performance, mean_hd95, mean_jacard))
     return "Testing Finished!"
 
 
@@ -177,7 +176,7 @@ if __name__ == "__main__":
     print("Val length : ", len(X_val))
     
     print("Snapshot path: ", snapshot_path)
-
+    net = net.cuda()
     db_test = LITSTestDataset(X_test,Y_test,transform=None)
     inference_lits(args, net, db_test, test_save_path)
 
--- utils/utils.py
+++ utils/utils.py
@@ -167,6 +167,38 @@ def calculate_dice_percase(pred, gt):
     else:
         return 0
 
+def calculate_hd95_percase(pred, gt):
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    if pred.sum() > 0 and gt.sum()>0:
+        dice = metric.binary.hd95(pred, gt)
+        return dice
+    elif pred.sum() > 0 and gt.sum()==0:
+        return 0
+    else:
+        return 0
+
+def calculate_jaccard_percase(pred, gt):
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    if pred.sum() > 0 and gt.sum()>0:
+        dice = metric.binary.jc(pred, gt)
+        return dice
+    elif pred.sum() > 0 and gt.sum()==0:
+        return 1
+    else:
+        return 0
+
+def calculate_assd_percase(pred, gt):
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    if pred.sum() > 0 and gt.sum()>0:
+        dice = metric.binary.assd(pred, gt)
+        return dice
+    elif pred.sum() > 0 and gt.sum()==0:
+        return 0
+    else:
+        return 0
 
 def test_single_volume(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1, class_names=None):
     image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
@@ -240,6 +272,50 @@ def test_single_volume(image, label, net, classes, patch_size=[256, 256], test_s
         sitk.WriteImage(prd_itk, test_save_path + '/'+case + "_pred.nii.gz")
         sitk.WriteImage(img_itk, test_save_path + '/'+ case + "_img.nii.gz")
         sitk.WriteImage(lab_itk, test_save_path + '/'+ case + "_gt.nii.gz")
+    return metric_list
+
+
+def test_lits_single(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1):
+    image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
+
+    if len(image.shape) == 3:
+        prediction = np.zeros_like(label)
+        for ind in range(image.shape[0]):
+            slice = image[ind, :, :]
+            x, y = slice.shape[0], slice.shape[1]
+            if x != patch_size[0] or y != patch_size[1]:
+                slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=3)  # previous using 0
+            input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
+            net.eval()
+            with torch.no_grad():                
+                P = net(input)
+                outputs = 0.0
+                for idx in range(len(P)):
+                   outputs += P[idx]
+                out = torch.argmax(torch.softmax(outputs, dim=1), dim=1).squeeze(0)
+                out = out.cpu().detach().numpy()
+                if x != patch_size[0] or y != patch_size[1]:
+                    pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
+                else:
+                    pred = out
+                prediction[ind] = pred
+    else:
+        input = torch.from_numpy(image).unsqueeze(
+            0).unsqueeze(0).float().cuda()
+        net.eval()
+        with torch.no_grad():
+            P = net(input)
+            outputs = 0.0
+            for idx in range(len(P)):
+               outputs += P[idx]
+            out = torch.argmax(torch.softmax(outputs, dim=1), dim=1).squeeze(0)
+            prediction = out.cpu().detach().numpy()
+    metric_list = []
+    for i in range(1, classes):
+      z1 = calculate_dice_percase(prediction == i, label == i)
+      z2 = calculate_hd95_percase(prediction == i, label == i)
+      z3 = calculate_jaccard_percase(prediction == i, label == i)
+      metric_list.append([z1,z2,z3])
     return metric_list
 
 
